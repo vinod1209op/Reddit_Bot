e755dc6a3aecbcf3f7e7c3114a3f92d1f8edc3dc
vinod1209op <62650750+vinod1209op@users.noreply.github.com>
2026-01-24 05:14:13 -0700

    errors

--- scripts/runners/humanized_night_scanner.py
+++ scripts/runners/humanized_night_scanner.py
@@ -879,10 +879,11 @@ class HumanizedNightScanner:
                     0.08,
                     0.12,
                 )
-                # Simulate navigation error after each action
-                if self.human_sim and random.random() < nav_error_chance:
-                    if self.human_sim.simulate_navigation_error(self.driver):
-                        actions_performed['navigation_errors'] += 1
+
+                if random.random() < nav_error_chance:
+                    self.humanization_metrics['navigation_errors'] += 1
+                    self.logger.debug("Simulating navigation error...")
+                    self.simulate_navigation_error()
                 
                 # Increment action counter
                 self.action_count += 1
